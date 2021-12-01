ac5ab5e18434b000223c9775650b93d42f58cbd4
dylan fair <dylanfair11@gmail.com>
2021-11-30 20:31:09 -0600

    added models

---
+++ models/reviews.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Review extends Model {}
+
+Item.init({
+    id: {
+        type:DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    context: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [0, 50]
+        }
+    },
+    number: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+},
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+}
+
+)
+
+module.exports = Review;
