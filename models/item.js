26ad850b368ff0e753b3bd368965f86ecb40c5b2
Victor Smith <victorj500@gmail.com>
2021-12-01 21:16:01 -0600

    formatting and capitalizing file

--- models/item.js
+++ models/item.js
@@ -1,42 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Item extends Model {}
 
-Item.init({
+Item.init(
+  {
     id: {
-        type:DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    item_text:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate:{
-            len: [1]
-        }
+    item_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
     },
-    user_id:{
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references:{
-         model: 'user',
-         key: 'id'
-    }
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
     },
-},
-{
+  },
+  {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'item'
-}
-
-)
+    modelName: "item",
+  }
+);
 
 module.exports = Item;
